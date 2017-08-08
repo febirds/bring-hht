9f71525319dd06359b39df1395698262a87b3cec
febirds <febirds52086@gmail.com>
2017-08-08 10:57:21 +0800

    ip modify

--- bring-hht-web/src/main/java/gv/hht/web/ws/CalculatorServiceLocator.java
+++ bring-hht-web/src/main/java/gv/hht/web/ws/CalculatorServiceLocator.java
@@ -22,7 +22,7 @@ public class CalculatorServiceLocator extends org.apache.axis.client.Service imp
     }
 
     // Use to get a proxy class for CalculatorPort
-    private String CalculatorPort_address = "http://6nh7cr.natappfree.cc/WebOnlineChargePro/CalculatorPort";
+    private String CalculatorPort_address = "http://6nh7cr.natappfree.cc/WebChargePro/CalculatorPort";
 
     public String getCalculatorPortAddress() {
         return CalculatorPort_address;
